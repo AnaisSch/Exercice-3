59f0bce053ec03091c9a2cc248cf72dcbf43458e
AnaisSch <60514726+AnaisSch@users.noreply.github.com>
2020-04-03 15:35:25 +0200

    ajout nav a droite et pills avec Nav.Link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,21 +2,23 @@ import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 
+
 const Navigation = () => {
     return (
-        <Nav>
+        
+        <Nav className="justify-content-end" variant="pills" defaultActiveKey="/">
             <Nav.Item>
-                <Nav.Link as={Link} to="/">
+                <Nav.Link as={Link} to="/" href="/">
                     Accueil
                 </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link as={Link} to="/comments/create">
+                <Nav.Link as={Link} to="/comments/create" eventKey="/comments/create">
                     Créer un commentaire
                 </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link as={Link} to="/comments/delete">
+                <Nav.Link as={Link} to="/comments/delete" eventKey="/comments/delete">
                     Supprimer un commentaire
                 </Nav.Link>
             </Nav.Item>
