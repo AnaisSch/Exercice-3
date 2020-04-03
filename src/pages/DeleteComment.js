1ab41f7a1028297ed4c2942719a0213dcefb93e9
AnaisSch <60514726+AnaisSch@users.noreply.github.com>
2020-04-03 12:52:03 +0200

    ajout bootstrap DeleteComment

--- src/pages/DeleteComment.js
+++ src/pages/DeleteComment.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const DeleteComment = () => {
+    const [id, setId] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log("id : ", id);
+    }
+
+    const handleChange = (event) => {
+        switch (event.target.name) {
+            case "id":
+                setId(event.target.value);
+                break;
+            // no default
+        }
+    }
+
     return (
-        <form>
-            <input
-                type="number"
-                name="id"
-                placeholder="Id du commentaire"
-            />
-            <button type="submit">Supprimer un commentaire</button>
-        </form>
+    <Container>
+        <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="commentId">
+                <Form.Label>Id du commentaire</Form.Label>
+                <Form.Control
+                    type="number"
+                        name="id"
+                        onChange={handleChange}
+                        value={id}
+                />
+            </Form.Group>
+            <Button variant="danger" type="submit">Supprimer un commentaire</Button>
+        </Form>
+    </Container>
+
     );
 }
 
