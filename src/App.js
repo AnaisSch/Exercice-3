96ccf0fc5355e397a8d0d96dc1d90c88dbc21211
AnaisSch <60514726+AnaisSch@users.noreply.github.com>
2020-04-03 12:45:14 +0200

    Ajout Bootstrap + Router + formulaire refait avec bootstrap dans CreateComment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import Home from './pages/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
-function App() {
+
+import Navigation    from './components/Navigation';
+import Home          from './pages/Home';
+import CreateComment from './pages/CreateComment';
+import DeleteComment from './pages/DeleteComment';
+import NotFound      from './pages/NotFound';
+
+const App = () => {
   return (
-    <div >
-        < Home /> 
-    </div>
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route exact path="/"           component={Home} />
+        <Route path="/comments/create/" component={CreateComment} />
+        <Route path="/comments/delete/" component={DeleteComment} />
+        <Route path="*"                 component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
--- src/pages/CreateComment.js
+++ src/pages/CreateComment.js
@@ -1,26 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 
 const CreateComment = () => {
-    return (
-        <form>
-            <input
-                type="text"
-                name="article_id"
-                placeholder="Id de l'article"
-            />
-            <textarea
-                type="textarea"
-                name="content"
-                placeholder="Contenu du commentaire"
-            >
-            </textarea>
-            <input
-                type="number"
-                name="author"
-                placeholder="Id de l'auteur"
-            />
-            <button type="submit">Créer un commentaire</button>
-        </form>
+    const [articleId, setArticleId] = useState("");
+    const [content, setContent] = useState("");
+    const [author, setAuthor] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("article_id", articleId);
+        console.log("content", content);
+        console.log("author", author);
+    }
+    const handleChange = (event) => {
+        console.log("Target name: ", event.target.name);
+        console.log("Target value: ", event.target.value);
+
+        switch (event.target.name) {
+            case "article_id":
+                setArticleId(event.target.value);
+                break;
+            case "content":
+                setContent(event.target.value);
+                break;
+            case "author":
+                setAuthor(event.target.value);
+                break;
+            // no default
+        }
+    }
+    return ( 
+        <Container>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="article.id">
+                <Form.Label> Identifiant de l'article</Form.Label>
+                <Form.Control
+                    type="text"
+                    name="article_id"
+                    onChange={handleChange}
+                    value={articleId}
+                    />
+                </Form.Group>
+                <Form.Group controlId="comment.content">
+                <Form.Label>Contenu du commentaire</Form.Label>
+                <Form.Control
+                    type="textarea"
+                    name="content"
+                    onChange={handleChange}
+                    value={content}
+                    />
+                </Form.Group>
+                <Form.Group controlId="comment.author">
+                <Form.Label>Id du commentaire</Form.Label>
+                <Form.Control
+                    type="number"
+                    name="author"
+                    onChange={handleChange}
+                    value={author}
+                    placeholder="Id de l'auteur"
+                        />
+                </Form.Group>
+            <Button variant="warning" type="submit">Créer un commentaire</Button>
+            </Form>
+        </Container>
     );
 }
 
