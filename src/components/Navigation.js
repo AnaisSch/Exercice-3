fc7d39bf05efc0de80a018bc51bf11748daa5a93
AnaisSch <60514726+AnaisSch@users.noreply.github.com>
2020-04-04 11:22:41 +0200

    Ajout de NavLink + pills

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 const Navigation = () => {
     return (
         
-        <Nav className="justify-content-end" variant="pills" defaultActiveKey="/">
+        <Nav className="justify-content-end" variant="pills" >
             <Nav.Item>
-                <Nav.Link as={Link} to="/" href="/">
+                <Nav.Link as={NavLink} exact={true} to="/">
                     Accueil
                 </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link as={Link} to="/comments/create" eventKey="/comments/create">
+                <Nav.Link as={NavLink} to="/comments/create">
                     Créer un commentaire
                 </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link as={Link} to="/comments/delete" eventKey="/comments/delete">
+                <Nav.Link as={NavLink} to="/comments/delete">
                     Supprimer un commentaire
                 </Nav.Link>
             </Nav.Item>
